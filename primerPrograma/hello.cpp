888de8e06d7a9e7e95e8e60c9d46a5696a54b44e
GerlyUwU <arteaga.bernal4@gmail.com>
2023-09-14 23:35:36 -0600

    inicializamos el repositorio de los  ejercicios en c++ que realizare durante el curso del mismo

---
+++ primerPrograma/hello.cpp
@@ -0,0 +1,21 @@
+// lectura o entrada de datos
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+/*
+hola que tal 
+mi nombre 
+es francisco vazques
+*/
+{
+    int numero; 
+
+    cout<<"digite un numero";
+    cin>>numero;
+    cout<<"\nEl numero que acaba de digitar es: "<<numero<<endl;
+
+    return 0;
+}
