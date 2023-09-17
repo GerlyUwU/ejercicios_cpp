dd40798d371f061690ac71aaafd3bb6838831a0f
GerlyUwU <arteaga.bernal4@gmail.com>
2023-09-16 20:43:32 -0600

    agregue un archivo de texto donde guardare los ejercicios simples de aprendizaje de sintaxis

--- primerPrograma/hello.cpp
+++ primerPrograma/hello.cpp
@@ -1,21 +1,23 @@
-// lectura o entrada de datos
+// Escribe la siguiente expresión como expresion en c++:
+//(a/b) +1
 
 #include <iostream>
 
 using namespace std;
 
 int main()
-/*
-hola que tal 
-mi nombre 
-es francisco vazques
-*/
 {
-    int numero; 
+    float a, b, resultado = 0;
 
-    cout<<"digite un numero";
-    cin>>numero;
-    cout<<"\nEl numero que acaba de digitar es: "<<numero<<endl;
+    cout << "ingrese el valor de a:" << endl;
+    cin >> a;
+    cout << "ingrese el valor de b:";
+    cin >> b;
+    resultado = (a / b) + 1;
+    cout << "EL RESULTADO DE (a/b)+1 es:" << endl;
+    //metodo de la clase cout para redondear los decimales segun el numero que le pases de parametro
+    cout.precision(2);
+    cout << resultado << endl;
 
     return 0;
 }
