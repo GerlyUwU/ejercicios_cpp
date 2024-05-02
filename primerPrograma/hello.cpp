41a563e5f9a1622ba77155acc02033360429edb4
GERLY DANIEL ARTEAGA BERNAL <arteaga.bernal3@gmail.com>
2024-05-02 01:43:34 -0600

    last commit incoming xd

--- primerPrograma/hello.cpp
+++ primerPrograma/hello.cpp
@@ -1,71 +1,21 @@
-/*
-ejercicio 11:
-            hacer un programa que simule un cajero
-            automatico con un saldo inicial del 1000 dolares
-*/
-
 #include <iostream>
 using namespace std;
+#include<stdlib.h>
 
-int main()
-{
-    int saldo = 1000;
-    int menu;
-    cout << "Bienvenido al banco" << endl;
-    cout << "A continuación, digite un número para elegir la opción a realizar:" << endl;
+int main(){
+char nombre[10];
+int edad;
+cout<<"ingresa tu nombre:\n "<<endl;
+cin>>nombre;
+cout<<"ingresa tu edad: \n"<<endl;
+cin>>edad;
 
-    cout << "1.- Retirar dinero" << endl;
-    cout << "2.- Depositar dinero" << endl;
-    cout << "3.- Consultar saldo" << endl;
-    cout << "4.- Salir" << endl;
-    cin >> menu;
+cout<<"hola,tu nombre es: "<<edad<<endl;
+cout<<"\n y tu edad es: "<<nombre<<endl;
 
-    switch (menu)
-    {
-    case 1:
-    {
-        int retiro = 0;
-        cout << "Ingrese la cantidad a retirar: ";
-        cin >> retiro;
-        if (retiro > saldo)
-        {
-            cout << "No tiene el saldo suficiente para realizar esta transacción. Labure más." << endl;
-            break;
-        }
-        else
-        {
-            saldo -= retiro;
-            cout << "Retiro exitoso. El saldo restante es: $" << saldo << ". ¡Vuelva pronto!" << endl;
-        }
-        break;
-    }
+cout<<"\n\n";
+system("pause");
 
-    case 2:
-    {
-        int deposito = 0;
-        cout << "Ingrese la cantidad a depositar: ";
-        cin >> deposito;
-        if (deposito > 1000000)
-        {
-            cout << "Me parece que tendrás que declarar ante el SAT esa cantidad. ._. " << endl;
-            break;
-        }
-        else
-        {
-            saldo += deposito;
-            cout << "Depósito exitoso. El saldo actualizado es de: $" << saldo << ". ¡Vuelva pronto!" << endl;
-        }
-        break;
-    }
-
-    case 3:
-        cout << "El saldo actual de la cuenta es: $" << saldo << ". ¡Vuelva pronto!" << endl;
-        break;
-    case 4:
-        cout << "Vuelva pronto. ¡Gracias por usar este servicio!" << endl;
-        break;
-    default:
-        cout << "Vaya, parece que no ingresaste un número válido. Vuelvelo a intentar. :)" << endl;
-    }
-    return 0;
+return 0;
 }
+
